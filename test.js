61e52405f63d0f04f32543daba3b35f591fe83bd
Dan VerWeire <dverweire@gmail.com>
2014-12-05 11:49:25 -0500

    add test.js

---
+++ test.js
@@ -0,0 +1,31 @@
+var test = require('tape')
+	, tpl = require('./')
+	;
+
+test('test render call', function (t) {
+	var str = tpl.render('${name}', { name : 'steve' });
+	t.equal(str, 'steve');
+	t.end();
+});
+
+test('test compile call', function (t) {
+	var c = tpl.compile('${name}');
+
+	var d = c({ name : 'steve' });
+
+	t.equal(d, 'steve');
+	t.end();
+});
+
+test('test filters/modifiers', function (t) {
+	tpl.filters.toYesNo = function (val) {
+		return (val) ? 'yes' : 'no';
+	}
+
+	var str1 = tpl.render('${good|toYesNo}', { good : true });
+	var str2 = tpl.render('${good|toYesNo}', { good : false });
+
+	t.equal(str1, 'yes');
+	t.equal(str2, 'no');
+	t.end();
+});
